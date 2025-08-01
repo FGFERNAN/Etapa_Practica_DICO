373b124fc5c7e2ec4f786c7206a27bf50d614efb
FGFERNAN <fgfernan2508@gmail.com>
2025-07-31 23:30:57 -0500

    Desglose de layouts y mockups de CRUD de productos

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,3 +1,5 @@
-<?php
-$this->load->view('layout/navbar');
-?>
+<?php $this->load->view('layout/header'); ?>
+<?php $this->load->view('layout/navbar'); ?>
+
+
+<?php $this->load->view('layout/footer'); ?>
